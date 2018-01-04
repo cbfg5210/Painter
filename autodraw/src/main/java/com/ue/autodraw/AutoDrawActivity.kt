4c616cc31810b324f8f57d8d61e94d0037a82bd2
tom <120276969@qq.com>
2018-01-04 10:28:12 +0800

    添加debug manifest文件和java目录

---
+++ autodraw/src/main/java/com/ue/autodraw/AutoDrawActivity.kt
@@ -0,0 +1,12 @@
+package com.ue.autodraw
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AutoDrawActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auto_draw)
+    }
+}
