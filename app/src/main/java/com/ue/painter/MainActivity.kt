4356b9d8f7da12200e2b2513dc6e573dda4059dc
tom <120276969@qq.com>
2018-01-04 11:27:24 +0800

    运行project

--- app/src/main/java/com/ue/painter/MainActivity.kt
+++ app/src/main/java/com/ue/painter/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.ue.painter
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+        }
     }
 }
