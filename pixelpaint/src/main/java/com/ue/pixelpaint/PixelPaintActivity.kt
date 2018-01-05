a41b4ace689b95aff5910d6b0aab74a9983893e3
tom <120276969@qq.com>
2018-01-05 11:06:12 +0800

    PixelPaint gesture event

--- pixelpaint/src/main/java/com/ue/pixelpaint/PixelPaintActivity.kt
+++ pixelpaint/src/main/java/com/ue/pixelpaint/PixelPaintActivity.kt
@@ -2,11 +2,18 @@ package com.ue.pixelpaint
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.ue.pixelpaint.gesture.MultiTouchListener
+import kotlinx.android.synthetic.main.activity_pixel_paint.*
 
 class PixelPaintActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pixel_paint)
+
+        val listener = MultiTouchListener()
+        listener.isRotateEnabled = false
+
+        view.setOnTouchListener(listener)
     }
 }
