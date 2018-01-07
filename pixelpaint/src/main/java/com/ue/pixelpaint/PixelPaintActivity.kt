5e6e61ec648553aeac8b07c7578193b6d3bb10fc
tom <120276969@qq.com>
2018-01-07 16:07:18 +0800

    delete MultiTouchListener

--- pixelpaint/src/main/java/com/ue/pixelpaint/PixelPaintActivity.kt
+++ pixelpaint/src/main/java/com/ue/pixelpaint/PixelPaintActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_pixel_paint.*
 
 class PixelPaintActivity : AppCompatActivity() {
     private var isTranslateEnabled = false
@@ -13,7 +12,7 @@ class PixelPaintActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pixel_paint)
 
-        pixelView.setTranslateEnabled(isTranslateEnabled)
+//        pixelView.setTranslateEnabled(isTranslateEnabled)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -24,7 +23,7 @@ class PixelPaintActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.actionPixelTranslate) {
             isTranslateEnabled = !isTranslateEnabled
-            pixelView.setTranslateEnabled(isTranslateEnabled)
+//            pixelView.setTranslateEnabled(isTranslateEnabled)
             return true
         }
         return super.onOptionsItemSelected(item)
--- pixelpaint/src/main/java/com/ue/pixelpaint/PixelView.kt
+++ pixelpaint/src/main/java/com/ue/pixelpaint/PixelView.kt
@@ -3,13 +3,16 @@ package com.ue.pixelpaint
 import android.content.Context
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import com.ue.pixelpaint.gesture.ScaleGestureDetector
+import com.ue.pixelpaint.gesture.Vector2D
 
 
 /**
  * Created by hawk on 2018/1/4.
  */
-class PixelView : View {
+class PixelView : View, View.OnTouchListener {
     var lineNum = 16
     private var linePaint: Paint = Paint()
     private var squareSize = 0F
@@ -19,14 +22,22 @@ class PixelView : View {
     private var thirdOneRatio = 0F
     private var thirdTwoRatio = 0F
 
+    private val INVALID_POINTER_ID = -1
+    var isTranslateEnabled = true
+    var isScaleEnabled = true
+    var minimumScale = 1f
+    var maximumScale = 10f
+    private var mActivePointerId = INVALID_POINTER_ID
+    private var mPrevX = 0f
+    private var mPrevY = 0f
+    private val mScaleGestureDetector = ScaleGestureDetector(ScaleGestureListener())
+
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle) {
 //        thirdOneRatio = resources.getDimension(R.dimen.widget_size_1) / 3
 //        thirdTwoRatio = resources.getDimension(R.dimen.widget_size_2) / 3
-    }
-
-    fun setTranslateEnabled(isTranslateEnabled: Boolean) {
+        setOnTouchListener(this)
     }
 
 /*    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -71,4 +82,135 @@ class PixelView : View {
             i += 8
         }
     }*/
+
+    private fun move(view: View, info: TransformInfo) {
+        computeRenderOffset(view, info.pivotX, info.pivotY)
+        adjustTranslation(view, info.deltaX, info.deltaY)
+
+        // Assume that scaling still maintains aspect ratio.
+        var scale = view.scaleX * info.deltaScale
+        scale = Math.max(info.minimumScale, Math.min(info.maximumScale, scale))
+        view.scaleX = scale
+        view.scaleY = scale
+    }
+
+    private fun adjustTranslation(view: View, deltaX: Float, deltaY: Float) {
+        val deltaVector = floatArrayOf(deltaX, deltaY)
+        view.matrix.mapVectors(deltaVector)
+        view.translationX = view.translationX + deltaVector[0]
+        view.translationY = view.translationY + deltaVector[1]
+    }
+
+    private fun computeRenderOffset(view: View, pivotX: Float, pivotY: Float) {
+        if (view.pivotX == pivotX && view.pivotY == pivotY) {
+            return
+        }
+
+        val prevPoint = floatArrayOf(0.0f, 0.0f)
+        view.matrix.mapPoints(prevPoint)
+
+        view.pivotX = pivotX
+        view.pivotY = pivotY
+
+        val currPoint = floatArrayOf(0.0f, 0.0f)
+        view.matrix.mapPoints(currPoint)
+
+        val offsetX = currPoint[0] - prevPoint[0]
+        val offsetY = currPoint[1] - prevPoint[1]
+
+        view.translationX = view.translationX - offsetX
+        view.translationY = view.translationY - offsetY
+    }
+
+    override fun onTouch(view: View, event: MotionEvent): Boolean {
+        mScaleGestureDetector.onTouchEvent(view, event)
+
+        if (!isTranslateEnabled) {
+            return true
+        }
+
+        val action = event.action
+        when (action and event.actionMasked) {
+            MotionEvent.ACTION_DOWN -> {
+                mPrevX = event.x
+                mPrevY = event.y
+
+                // Save the ID of this pointer.
+                mActivePointerId = event.getPointerId(0)
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                // Find the index of the active pointer and fetch its position.
+                val pointerIndex = event.findPointerIndex(mActivePointerId)
+                if (pointerIndex != -1) {
+                    val currX = event.getX(pointerIndex)
+                    val currY = event.getY(pointerIndex)
+
+                    // Only move if the ScaleGestureDetector isn't processing a
+                    // gesture.
+                    if (!mScaleGestureDetector.isInProgress) {
+                        adjustTranslation(view, currX - mPrevX, currY - mPrevY)
+                    }
+                }
+            }
+
+            MotionEvent.ACTION_CANCEL -> mActivePointerId = INVALID_POINTER_ID
+
+            MotionEvent.ACTION_UP -> mActivePointerId = INVALID_POINTER_ID
+
+            MotionEvent.ACTION_POINTER_UP -> {
+                // Extract the index of the pointer that left the touch sensor.
+                val pointerIndex = action and MotionEvent.ACTION_POINTER_INDEX_MASK shr MotionEvent.ACTION_POINTER_INDEX_SHIFT
+                val pointerId = event.getPointerId(pointerIndex)
+                if (pointerId == mActivePointerId) {
+                    // This was our active pointer going up. Choose a new
+                    // active pointer and adjust accordingly.
+                    val newPointerIndex = if (pointerIndex == 0) 1 else 0
+                    mPrevX = event.getX(newPointerIndex)
+                    mPrevY = event.getY(newPointerIndex)
+                    mActivePointerId = event.getPointerId(newPointerIndex)
+                }
+            }
+        }
+
+        return true
+    }
+
+    private inner class ScaleGestureListener : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+
+        private var mPivotX = 0f
+        private var mPivotY = 0f
+        private val mPrevSpanVector = Vector2D()
+
+        override fun onScaleBegin(view: View, detector: ScaleGestureDetector): Boolean {
+            mPivotX = detector.focusX
+            mPivotY = detector.focusY
+            mPrevSpanVector.set(detector.currentSpanVector)
+            return true
+        }
+
+        override fun onScale(view: View, detector: ScaleGestureDetector): Boolean {
+            val info = TransformInfo()
+            info.deltaScale = if (isScaleEnabled) detector.scaleFactor else 1.0f
+            info.deltaX = if (isTranslateEnabled) detector.focusX - mPivotX else 0.0f
+            info.deltaY = if (isTranslateEnabled) detector.focusY - mPivotY else 0.0f
+            info.pivotX = mPivotX
+            info.pivotY = mPivotY
+            info.minimumScale = minimumScale
+            info.maximumScale = maximumScale
+
+            move(view, info)
+            return false
+        }
+    }
+
+    private inner class TransformInfo {
+        var deltaX = 0f
+        var deltaY = 0f
+        var deltaScale = 0f
+        var pivotX = 0f
+        var pivotY = 0f
+        var minimumScale = 0f
+        var maximumScale = 0f
+    }
 }
