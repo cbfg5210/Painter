3e2dfa932aaf44ce1f672aaf1d634df6693f263c
tom <120276969@qq.com>
2018-01-08 21:04:16 +0800

    暂时使用view,因为surfaceView缩放有问题，surfaceView缩放的是view层，没有缩放surface层，后续再修改

--- pixelpaint/src/main/java/com/ue/pixelpaint/PixelView.kt
+++ pixelpaint/src/main/java/com/ue/pixelpaint/PixelView.kt
@@ -6,14 +6,13 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.Log
-import android.view.SurfaceHolder
-import android.view.SurfaceView
+import android.view.View
 
 
 /**
  * Created by hawk on 2018/1/4.
  */
-class PixelView : SurfaceView, SurfaceHolder.Callback  {
+class PixelView : View {
 
     var lineNum = 16
     private var linePaint: Paint = Paint()
@@ -24,17 +23,12 @@ class PixelView : SurfaceView, SurfaceHolder.Callback  {
     private var thirdOneRatio = 0F
     private var thirdTwoRatio = 0F
 
-    private lateinit var renderThread: Thread
-    private var isRenderEnabled: Boolean = false//是否继续渲染
-
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle) {
         thirdOneRatio = resources.getDimension(R.dimen.widget_size_1) * 0.3f
         thirdTwoRatio = resources.getDimension(R.dimen.widget_size_2) * 0.3f
         setOnTouchListener(GestureTouchListener())
-
-        holder.addCallback(this)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -42,14 +36,10 @@ class PixelView : SurfaceView, SurfaceHolder.Callback  {
         Log.e("PixelView", "onMeasure: w=$measuredWidth,h=$measuredHeight")
     }
 
-//    override fun setScaleX(scaleX: Float) {
-//        super.setScaleX(scaleX)
-//        val canvas = holder.lockCanvas()
-//        val matrix=Matrix()
-//        matrix.postScale(scaleX,scaleX)
-//        canvas.matrix=matrix
-//        holder.unlockCanvasAndPost(canvas)
-//    }
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        render(canvas)
+    }
 
     private fun render(canvas: Canvas) {
         canvas.drawColor(Color.WHITE)
@@ -81,52 +71,6 @@ class PixelView : SurfaceView, SurfaceHolder.Callback  {
             canvas.drawLine(tempSize, 0F, tempSize, canvasSize, linePaint)
 
             i += 8
-        }
-    }
-
-    private val renderRunnable = Runnable {
-        while (isRenderEnabled) {
-            var canvas: Canvas? = null
-
-            try {
-                canvas = holder.lockCanvas()
-                synchronized(holder) {
-                    render(canvas!!)
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            } finally {
-                if (canvas != null) holder.unlockCanvasAndPost(canvas)
-            }
-
-            try {
-                Thread.sleep(1000)
-            } catch (e: InterruptedException) {
-                e.printStackTrace()
-            }
-        }
-    }
-
-    /*
-    * surface callback
-    * */
-    override fun surfaceCreated(holder: SurfaceHolder) {
-        renderThread = Thread(renderRunnable)
-        isRenderEnabled = true
-        renderThread.start()
-    }
-
-    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
-        Log.e("PixelView", "surfaceChanged: w=$width,h=$height")
-    }
-
-    override fun surfaceDestroyed(holder: SurfaceHolder?) {
-        isRenderEnabled = false
-        holder?.removeCallback(this)
-        try {
-            renderThread.join()
-        } catch (e: InterruptedException) {
-            e.printStackTrace()
         }
     }
 }
