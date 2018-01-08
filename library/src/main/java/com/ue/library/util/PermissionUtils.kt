14470e497a9bbadd86c58e11e99c5aa1d48ff04c
tom <120276969@qq.com>
2018-01-08 21:44:40 +0800

    add PermissionUtils

---
+++ library/src/main/java/com/ue/library/util/PermissionUtils.kt
@@ -0,0 +1,33 @@
+package com.ue.library.util
+
+import android.content.Context
+import com.yanzhenjie.permission.AndPermission
+import com.yanzhenjie.permission.PermissionListener
+
+/**
+ * Created by hawk on 2018/1/8.
+ */
+object PermissionUtils {
+    fun checkPermission(context: Context, reqCode: Int, perms: Array<String>, callback: PermissionListener?) {
+        AndPermission.with(context)
+                .requestCode(reqCode)
+                .permission(perms)
+                .rationale { _, rationale ->
+                    AndPermission.rationaleDialog(context, rationale).show()
+                }
+                .callback(object : PermissionListener {
+                    override fun onSucceed(requestCode: Int, grantPermissions: MutableList<String>) {
+                        if (requestCode == reqCode) {
+                            callback?.onSucceed(requestCode, grantPermissions)
+                        }
+                    }
+
+                    override fun onFailed(requestCode: Int, deniedPermissions: MutableList<String>) {
+                        if (requestCode == reqCode) {
+                            callback?.onFailed(requestCode, deniedPermissions)
+                        }
+                    }
+                })
+                .start()
+    }
+}
