7008a1f46840923189e5909c3d493fbbc491e7dc
tom <120276969@qq.com>
2018-01-24 14:40:57 +0800

    edit:TextImageView

--- graffiti/src/main/java/com/ue/graffiti/widget/TextImageView.kt
+++ graffiti/src/main/java/com/ue/graffiti/widget/TextImageView.kt
@@ -12,18 +12,20 @@ import com.ue.graffiti.model.Text
 import com.ue.graffiti.touch.TextImageTouch
 import com.ue.graffiti.util.TouchUtils
 
-class TextImageView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr) {
+class TextImageView : View {
     private var savedBitmap: Bitmap? = null
-    var touch: TextImageTouch? = null
+    lateinit var touch: TextImageTouch
         private set
-    private var textPoint: PointF? = null//文字坐标
-    private var centerPoint: PointF? = null//文字中心
+    //文字坐标
+    private lateinit var textPoint: PointF
+    //文字中心
+    private lateinit var centerPoint: PointF
 
     private var contentId: Int = 0
     var imageContent: Bitmap? = null
         private set
 
-    private var drawTextPaint: Paint? = null
+    private lateinit var drawTextPaint: Paint
     private var textContent = ""
 
     private var mBarSensorListener: BarSensorListener? = null
@@ -32,25 +34,26 @@ class TextImageView @JvmOverloads constructor(context: Context, attrs: Attribute
 
     val picture: Picture
         get() = Picture(contentId,
-                touch!!.dx, touch!!.dy, touch!!.scale, touch!!.degree,
-                PointF(centerPoint!!.x, centerPoint!!.y),
-                PointF(textPoint!!.x, textPoint!!.y))
+                touch.dx, touch.dy, touch.scale, touch.degree,
+                PointF(centerPoint.x, centerPoint.y),
+                PointF(textPoint.x, textPoint.y))
 
-    init {
-
+    constructor(context: Context) : this(context, null, 0)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         val ta = context.obtainStyledAttributes(attrs, R.styleable.TextImageView)
         type = ta.getInt(R.styleable.TextImageView_type, 0)
+        ta.recycle()
         if (type == 0) {
             //text
             drawTextPaint = Paint()
-            drawTextPaint!!.textSize = 50f
-            drawTextPaint!!.typeface = Typeface.DEFAULT_BOLD
+            drawTextPaint.textSize = 50f
+            drawTextPaint.typeface = Typeface.DEFAULT_BOLD
         } else {
             //image
             textPoint = PointF()
             centerPoint = PointF()
         }
-        ta.recycle()
     }
 
     fun setBarSensorListener(barSensorListener: BarSensorListener) {
@@ -58,7 +61,7 @@ class TextImageView @JvmOverloads constructor(context: Context, attrs: Attribute
     }
 
     fun setTextColor(color: Int) {
-        drawTextPaint!!.color = color
+        drawTextPaint.color = color
         invalidate()
     }
 
@@ -67,40 +70,40 @@ class TextImageView @JvmOverloads constructor(context: Context, attrs: Attribute
 
         touch = TextImageTouch(true, canvasWidth, canvasHeight)
         if (mBarSensorListener != null) {
-            touch!!.setBarSensorListener(mBarSensorListener!!)
+            touch.setBarSensorListener(mBarSensorListener!!)
         }
 
-        drawTextPaint!!.color = paintColor
+        drawTextPaint.color = paintColor
 
         textPoint = PointF()
-        textPoint!!.set(touch!!.textPoint)
+        textPoint.set(touch.textPoint)
         centerPoint = PointF()
-        centerPoint!!.set(touch!!.centerPoint)
+        centerPoint.set(touch.centerPoint)
     }
 
     fun setBitmap(savedBitmap: Bitmap, canvasWidth: Int, canvasHeight: Int) {
         this.savedBitmap = Bitmap.createScaledBitmap(savedBitmap, canvasWidth, canvasHeight, true)
 
-        textPoint!!.set(canvasWidth / 2.5f, canvasHeight / 2.5f)
-        centerPoint!!.set(textPoint)
+        textPoint.set(canvasWidth / 2.5f, canvasHeight / 2.5f)
+        centerPoint.set(textPoint)
 
         touch = TextImageTouch(false, canvasWidth, canvasHeight)
         if (mBarSensorListener != null) {
-            touch!!.setBarSensorListener(mBarSensorListener!!)
+            touch.setBarSensorListener(mBarSensorListener!!)
         }
     }
 
     fun getText(paintColor: Int): Text {
         return Text(textContent,
-                touch!!.dx, touch!!.dy, touch!!.scale, touch!!.degree,
-                PointF(touch!!.centerPoint.x, touch!!.centerPoint.y),
-                PointF(touch!!.textPoint.x, touch!!.textPoint.y), paintColor)
+                touch.dx, touch.dy, touch.scale, touch.degree,
+                PointF(touch.centerPoint.x, touch.centerPoint.y),
+                PointF(touch.textPoint.x, touch.textPoint.y), paintColor)
     }
 
     //触摸事件
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        touch!!.setCurrentPoint(PointF(event.getX(0), event.getY(0)))
-        touch!!.setSecondPoint(if (event.pointerCount > 1) PointF(event.getX(1), event.getY(1)) else PointF(1f, 1f))
+        touch.setCurrentPoint(PointF(event.getX(0), event.getY(0)))
+        touch.setSecondPoint(if (event.pointerCount > 1) PointF(event.getX(1), event.getY(1)) else PointF(1f, 1f))
 
         val actionMasked = event.actionMasked
         when (actionMasked) {
@@ -108,18 +111,18 @@ class TextImageView @JvmOverloads constructor(context: Context, attrs: Attribute
             MotionEvent.ACTION_DOWN -> {
                 if (mBarSensorListener != null && mBarSensorListener!!.isTopToolbarVisible()) {
                     mBarSensorListener!!.closeTools()
-                    touch!!.setDis(java.lang.Float.MAX_VALUE)
+                    touch.setDis(java.lang.Float.MAX_VALUE)
                 }
-                touch!!.down1()
+                touch.down1()
             }
             MotionEvent.ACTION_POINTER_DOWN ->
                 // 第二个手指按下
-                touch!!.down2()
-            MotionEvent.ACTION_MOVE -> touch!!.move()
-        // 第一只手指抬起
+                touch.down2()
+            MotionEvent.ACTION_MOVE -> touch.move()
+                // 第一只手指抬起
             MotionEvent.ACTION_UP,
                 //第二只手抬起
-            MotionEvent.ACTION_POINTER_UP -> touch!!.up()
+            MotionEvent.ACTION_POINTER_UP -> touch.up()
         }
         invalidate()
 
@@ -128,19 +131,20 @@ class TextImageView @JvmOverloads constructor(context: Context, attrs: Attribute
 
     //重绘
     override fun onDraw(canvas: Canvas) {
-        canvas.drawBitmap(savedBitmap!!, 0f, 0f, Paint())// 画其余图元
-        canvas.translate(touch!!.dx, touch!!.dy)
-        canvas.scale(touch!!.scale, touch!!.scale, centerPoint!!.x, centerPoint!!.y)
-        canvas.rotate(touch!!.degree, centerPoint!!.x, centerPoint!!.y)
+        // 画其余图元
+        canvas.drawBitmap(savedBitmap, 0f, 0f, Paint())
+        canvas.translate(touch.dx, touch.dy)
+        canvas.scale(touch.scale, touch.scale, centerPoint.x, centerPoint.y)
+        canvas.rotate(touch.degree, centerPoint.x, centerPoint.y)
 
         if (type == 0) {
             //text
-            canvas.drawText(textContent, textPoint!!.x, textPoint!!.y, drawTextPaint!!)
+            canvas.drawText(textContent, textPoint.x, textPoint.y, drawTextPaint)
             return
         }
         //image
         if (imageContent != null) {
-            canvas.drawBitmap(imageContent!!, textPoint!!.x, textPoint!!.y, null)
+            canvas.drawBitmap(imageContent!!, textPoint.x, textPoint.y, null)
         }
     }
 
@@ -152,6 +156,6 @@ class TextImageView @JvmOverloads constructor(context: Context, attrs: Attribute
         this.contentId = contentId
         TouchUtils.ensureBitmapRecycled(imageContent)
         this.imageContent = BitmapFactory.decodeResource(context.resources, contentId)
-        centerPoint!!.set(textPoint!!.x + imageContent!!.width / 2, textPoint!!.y + imageContent!!.height / 2)
+        centerPoint.set(textPoint.x + imageContent!!.width / 2, textPoint.y + imageContent!!.height / 2)
     }
 }
