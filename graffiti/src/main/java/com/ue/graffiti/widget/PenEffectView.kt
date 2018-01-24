3eaf95af06233f58dc437f72b3e379bacc65a2f4
tom <120276969@qq.com>
2018-01-24 14:46:23 +0800

    edit:PenEffectView

--- graffiti/src/main/java/com/ue/graffiti/widget/PenEffectView.kt
+++ graffiti/src/main/java/com/ue/graffiti/widget/PenEffectView.kt
@@ -7,13 +7,16 @@ import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
-class PenEffectView(context: Context, attrs: AttributeSet) : View(context, attrs) {
-    private var path: Path? = null
-    private var mPaint: Paint? = null
+class PenEffectView : View {
+    private lateinit var path: Path
+    private var mPaint = Paint()
+
+    constructor(context: Context?) : this(context, null)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs, 0)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     init {
         initPath()
-        mPaint = Paint()
     }
 
     fun setPaint(newPaint: Paint) {
@@ -23,23 +26,22 @@ class PenEffectView(context: Context, attrs: AttributeSet) : View(context, attrs
 
     //以当前选中的笔触（粗细、特效）画在矩形示意框里
     override fun onDraw(canvas: Canvas) {
-        //        canvas.drawPath(path, DrawTouch.getCurPaint());
-        canvas.drawPath(path!!, mPaint!!)
+        canvas.drawPath(path, mPaint)
     }
 
-    fun initPath() {
+    private fun initPath() {
         path = Path()
 
         val width = resources.displayMetrics.widthPixels.toFloat()
         val height = 160f
 
-        path!!.moveTo(0f, height / 2)
-        path!!.cubicTo(0f, height / 2, width / 4, height / 4, width / 2, height / 2)
-
-        val path2 = Path() //下波浪 连接用
+        path.moveTo(0f, height / 2)
+        path.cubicTo(0f, height / 2, width / 4, height / 4, width / 2, height / 2)
+        //下波浪 连接用
+        val path2 = Path()
         path2.moveTo(width / 2, height / 2)
         path2.cubicTo(width / 2, height / 2, width / 4 * 3, height / 4 * 3, width, height / 2)
 
-        path!!.addPath(path2)
+        path.addPath(path2)
     }
 }
