df6ad9169e56b64d58702984e395db12e92d32e8
tom <120276969@qq.com>
2018-01-24 10:45:34 +0800

    edit:convert DrawLineTouch to kotlin

---
+++ graffiti/src/main/java/com/ue/graffiti/touch/DrawLineTouch.kt
@@ -0,0 +1,26 @@
+package com.ue.graffiti.touch
+
+import com.ue.graffiti.model.Pel
+import com.ue.graffiti.widget.CanvasView
+
+class DrawLineTouch(canvasView: CanvasView) : DrawTouch(canvasView) {
+
+    override fun move() {
+        super.move()
+
+        newPel = Pel()
+        movePoint.set(curPoint)
+
+        newPel.path.moveTo(downPoint.x, downPoint.y)
+        newPel.path.lineTo(movePoint.x, movePoint.y)
+        newPel.path.lineTo(movePoint.x, movePoint.y + 1)
+
+        selectedPel = newPel
+        mSimpleTouchListener.setSelectedPel(selectedPel)
+    }
+
+    override fun up() {
+        newPel.closure = false
+        super.up()
+    }
+}
