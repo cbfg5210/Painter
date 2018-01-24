1ac582a2c8dbd5f3e5b6f7ffc530fbc201030a59
tom <120276969@qq.com>
2018-01-24 10:43:24 +0800

    edit:convert DrawFreehandTouch to kotlin

---
+++ graffiti/src/main/java/com/ue/graffiti/touch/DrawFreehandTouch.kt
@@ -0,0 +1,33 @@
+package com.ue.graffiti.touch
+
+import android.graphics.PointF
+
+import com.ue.graffiti.model.Pel
+import com.ue.graffiti.widget.CanvasView
+
+
+class DrawFreehandTouch(canvasView: CanvasView) : DrawTouch(canvasView) {
+    private val lastPoint: PointF = PointF()
+
+    override fun down1() {
+        super.down1()
+        lastPoint.set(downPoint)
+
+        newPel = Pel()
+        newPel.path.moveTo(lastPoint.x, lastPoint.y)
+    }
+
+    override fun move() {
+        super.move()
+        movePoint.set(curPoint)
+        newPel.path.quadTo(lastPoint.x, lastPoint.y, (lastPoint.x + movePoint.x) / 2, (lastPoint.y + movePoint.y) / 2)
+        lastPoint.set(movePoint)
+        selectedPel = newPel
+        mSimpleTouchListener.setSelectedPel(selectedPel)
+    }
+
+    override fun up() {
+        newPel.closure = true
+        super.up()
+    }
+}
