23e4ab33c7358aadc874523c2f66c39b8efbd14a
tom <120276969@qq.com>
2018-01-25 09:58:10 +0800

    edit:support svg

--- graffiti/src/main/debug/java/com.ue.graffiti/DebugApplication.kt
+++ graffiti/src/main/debug/java/com.ue.graffiti/DebugApplication.kt
@@ -1,6 +1,7 @@
 package com.ue.graffiti
 
 import android.app.Application
+import android.support.v7.app.AppCompatDelegate
 import com.ue.library.util.SPUtils
 
 /**
@@ -10,6 +11,7 @@ import com.ue.library.util.SPUtils
 class DebugApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
         SPUtils.init(this)
     }
 }
